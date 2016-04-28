2b61267fa5556b2bb9238e3ccafbf78c6de37a71
Nguyen <1312393@student.hcmus.edu.vn>
2016-04-28 13:26:34 +0700

    Update password.js ....

--- models/users.js
+++ models/users.js
@@ -2,8 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
     email : String,
-    password : String,
-    fullName : String
+    hash : String,
+    salt : String,
+    fullName : String,
+    friend : [{
+                type: Schema.ObjectId,
+                ref: 'users'
+    }]
 });
 var user = mongoose.model('users', userSchema);
 module.exports = user;
